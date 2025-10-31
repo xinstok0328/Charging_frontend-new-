043fd0333dd756131b72be707a4fd3636b4bb29f
wilson <wilson052864@gmail.com>
2025-10-31 14:42:38 +0800

    feat: update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\CustomAuth;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpFoundation\Request as RequestAlias;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,15 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->validateCsrfTokens(
             except: ['*', 'api/*']
+        );
+        // 信任 ngrok／反向代理，讓 Laravel 正確把當前協議判為 https
+        $middleware->trustProxies(
+            at: '*',
+            headers:
+            RequestAlias::HEADER_X_FORWARDED_FOR |
+            RequestAlias::HEADER_X_FORWARDED_HOST |
+            RequestAlias::HEADER_X_FORWARDED_PORT |
+            RequestAlias::HEADER_X_FORWARDED_PROTO
         );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
